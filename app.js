ba067b76825417465f2c25b770e1edb903d5c776
Manorojas <manuel.cra@gmail.com>
2025-01-24 19:22:53 -0300

    Add files via upload
    primera subda

---
+++ app.js
@@ -0,0 +1,23 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//crear variables
+let amigosBuenos = [];
+let amigosElegidos
+
+while (amigosElegidos < 6) {
+    amigosBuenos
+
+    amigosElegidos++;
+}
+amigosBuenos.includes(amigosElegidos)
+
+
+
+
+//crear lista de 5 nombres de amigos añadidos, el listado se reflejará 
+// eben aparecer bajo la caja donde se dijite.
+
+
+
+//p´resionar el boton "SORTEAR AMIGO", el nombre debe aparecer en la caja donde los escribía
+
+
